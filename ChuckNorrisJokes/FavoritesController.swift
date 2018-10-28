135f4c8079236c539d3c54dd3c175cb15d757cef
ArseniyKolosha <35842248+ArseniyKolosha@users.noreply.github.com>
2018-10-28 19:29:09 +0200

    FavoritesCotroller

---
+++ ChuckNorrisJokes/FavoritesController.swift
@@ -0,0 +1,97 @@
+//
+//  FavoritesController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Ars on 10/28/18.
+//  Copyright © 2018 ArsenIT. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return jokes.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CelliD", for: indexPath)
+
+        cell.textLabel?.text = jokes[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let joke = jokes[indexPath.row]
+        let avc = UIActivityViewController(activityItems: [joke], applicationActivities: nil)
+        present(avc, animated: true, completion: nil)
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            jokes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ChuckNorrisJokes/Model.swift
+++ ChuckNorrisJokes/Model.swift
@@ -49,3 +49,4 @@ func addJokeToFavorites(newJoke: String)  {
     jokes.append(newJoke)
     
 }
+
